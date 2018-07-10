16f07275c369630ec376ffe429e9fbf4d1593271
Wedson Santos <wedson_cross@hotmail.com>
2018-07-10 14:18:24 -0300

    rota dashboard finalizado

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,19 +7,38 @@ use App\noticias;
 use App\guiaComercial;
 use App\visitas;
 use App\eventos;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(){
         
+        $now = Carbon::now();
+        
+        $mes = $now->month;
+        $ano = $now->year;
+        
+        $trafego = visitas::whereMonth('created_at', '=', $mes)->whereYear('created_at', '=', $ano, 'or')->orderBy('created_at', 'ASC')->get()->groupBy(function($date){
+            return Carbon::parse($date->created_at)->format('d');
+        });
+        
+        $visitas = collect([]);
+        foreach($trafego as $key => $val):
+            $visitas[$key] = $val->count();
+        endforeach;       
+              
         $resposta = [        
             "count" => [
                 'visitas' => visitas::count(),
                 'noticias' => noticias::count(),
                 'guia' => guiaComercial::count(),
                 'eventos' => eventos::count()   
-              ]
-        ];
+              ],
+            "trafego" => [
+                "dias" => $visitas->keys(),
+                "visitas" => $visitas->values()
+                ]
+        ];        
         
         return compact('resposta');
     }
